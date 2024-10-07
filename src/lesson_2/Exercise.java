a551b5706e369c461db007c910bc5c0c448d0a98
Andy <admin@3sch39.ru>
2024-10-07 21:10:54 +0200

    add lesson_2

---
+++ src/lesson_2/Exercise.java
@@ -0,0 +1,12 @@
+package lesson_2;
+
+public class Exercise {
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 45;
+        int c = 17;
+        int p = a + b + c;
+        System.out.println(p);
+
+    }
+}
---
+++ src/lesson_2/Main.java
@@ -0,0 +1,11 @@
+package lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+        byte a = 123;
+        System.out.println(a);
+        a = 50;
+        int b = 34;
+        System.out.println(a+b);
+    }
+}
